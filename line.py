49b669d47479f42c02da39005e810943f18ac6f1
morsivsky <morsy_mm@hotmail.fr>
2019-08-20 03:12:42 +0200

    isolation of input files

--- line.py
+++ line.py
@@ -1,3 +1,5 @@
+from InstructionSet import InstructionSet
+
 class Line:
     def __init__(self, address, lineStr, physicalLineNumber):
         self.address = address
@@ -31,5 +33,65 @@ class symbol:
         self.refs=refs
 
 
+class output:
+    def __init__(self,filePath,startAddress):
+        self.filePath = filePath
+        self.startAddress = startAddress
+        self.lines=[]
+        self.extDefs= {}
+        self.extRefs={}
+        self.symbolTable={}
+
+    def prepare(self):
+        f = open(self.filePath, 'r')
+        allLines = f.read().split('\n')
+        if 'org' not in allLines[0].tolower():
+            raise Exception("ORG Not Found")
+            
+        if 'extdef' in allLines[1].tolower():
+            defs = allLines[1].tolower() - 'extdef'
+            for df in defs.split(','):
+                self.extDefs[df.strip()] =None
+
+        if 'extref' in allLines[2].tolower():
+            refs = allLines[1].tolower() - 'extref'
+            refs = refs.split(',') 
+            for rf in refs:
+                self.extRefs[rf.strip()] = None
+            
+
+    def process(self):
+        f = open('example.txt', 'r')
+        allLines = f.read().split('\n')
+        symTable = {}
+        result = []
+        viritualLineNumber = 0
+        for i, line in enumerate(allLines):
+            if line =='' or line.isspace():
+                continue
+            newLine = Line( int(hex(viritualLineNumber), 16) + self.startAddress, line, i)
+            result.append(newLine)
+
+            if newLine.label != '':
+                symTable[newLine.label] = symbol(newLine.address,[])
+
+            viritualLineNumber += 1
+        
+        for line in result:
+            if line.operand in symTable.keys():
+                lbl = line.operand
+                line.hexOperand = symTable[lbl].address
+                symTable[lbl].refs.append(line.address)
+            if line.opcode.lower() == 'skipcond':
+                line.hexOperand =int(line.operand,16)
+
+            if line.opcode.lower() in ['hex','dec']:
+                line.hexaOutput = "0x{0:0=4X}".format(int(line.operand))
+            else:
+                line.hexaOutput = "0x{0:0=1X}{1:0=3X}".format(
+                    InstructionSet[line.opcode.lower()],line.hexOperand)
+
+
+        self.lines,self.symbolTable = result,symTable
    
 
